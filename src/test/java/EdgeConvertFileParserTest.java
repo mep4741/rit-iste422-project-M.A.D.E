54049c6ef551b32ee9c919284e7f91ec35a3607a
Declan Naughton <djn5293@rit.edu>
2024-03-31 19:34:21 -0400

    Consider this me giving up. I have no idea what I'm doing, and frankly doubt I ever will.

--- src/test/java/EdgeConvertFileParserTest.java
+++ src/test/java/EdgeConvertFileParserTest.java
@@ -1,3 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Arrays;
+
+
 public class EdgeConvertFileParserTest {
-    
+    EdgeConvertFileParser testObj;
+
+    @Before
+    public void setUp() throws Exception{
+        //Begin by testing a save file.
+        testObj = new EdgeConvertFileParser(null);
+    } //setUp
+
+    @Test
+    public void testParseSaveFile(){
+        
+    }
 }
