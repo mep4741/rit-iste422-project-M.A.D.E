126adfffae6c2112e11e25cdbc4c15f95fdb3430
Declan Naughton <djn5293@rit.edu>
2024-04-02 16:40:29 -0400

    Stubbed out some tests.

--- src/test/java/CreateDDLMySQLTest.java
+++ src/test/java/CreateDDLMySQLTest.java
@@ -5,10 +5,61 @@ import org.junit.Test;
 
 public class CreateDDLMySQLTest {
     private CreateDDLMySQL testObj;
+    private String testSQLString;
 
 
     @Before
     public void setUp() throws Exception{
         testObj = new CreateDDLMySQL(null, null);
+        testSQLString = testObj.getSQLString();
     }//setUp
+
+    @Test
+    public void testDefaultDatabaseName(){
+        assertEquals("Database Name recorded does not match expected default of \"MySQLDB\".","MySQLDB", testObj.getDatabaseName());
+    }
+
+    @Test
+    public void testSQLStringExistence(){
+        
+    }
+
+    @Test
+    public void testDatabaseCreation(){
+
+    }
+
+    @Test
+    public void testDatabaseUsed(){
+
+    }
+
+    @Test
+    public void testTableCreated(){
+
+    }
+
+    @Test
+    public void testTableFieldsExistence(){
+
+    }
+
+    @Test
+    public void testNotNullFields(){
+
+    }
+
+    @Test
+    public void testFieldDefaults(){
+
+    }
+
+    @Test
+    public void testPrimaryKey(){
+
+    }
+    @Test
+    public void testForeignKey(){
+
+    }
 }
