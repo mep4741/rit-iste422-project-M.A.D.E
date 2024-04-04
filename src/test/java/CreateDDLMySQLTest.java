3767915a49df22221df5615cc103c08a77e61a87
Declan Naughton <djn5293@rit.edu>
2024-04-04 14:10:41 -0400

    Ran some build commands.

--- src/test/java/CreateDDLMySQLTest.java
+++ src/test/java/CreateDDLMySQLTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Array;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,35 +10,41 @@ public class CreateDDLMySQLTest {
     private String testSQLString;
 
 
+    //These tests assume that EdgeField and EdgeTable all tested correctly.
     @Before
     public void setUp() throws Exception{
-        testObj = new CreateDDLMySQL(null, null);
+        EdgeTable testTableA = new EdgeTable("1|Table A");
+        EdgeTable tesTableB = new EdgeTable("2|Table B");
+        EdgeTable[] tables = new EdgeTable[]{
+            new EdgeTable("1|Table a|Field alpha|Field A"),
+            new EdgeTable("2|Table b|Field bravo|Field B")
+        };
+        EdgeField[] fields = new EdgeField[]{
+            new EdgeField("1|Field alpha"),
+            new EdgeField("2|Field bravo"),
+            new EdgeField("3|Field A"),
+            new EdgeField("4|Field B")
+        };
+        testObj = new CreateDDLMySQL(tables, fields);
+        testObj.createDDL();//Create String
         testSQLString = testObj.getSQLString();
     }//setUp
 
     @Test
-    public void testDefaultDatabaseName(){
-        assertEquals("Database Name recorded does not match expected default of \"MySQLDB\".","MySQLDB", testObj.getDatabaseName());
-    }
-
-    @Test
     public void testSQLStringExistence(){
-        
+        assertNotNull("SQLString should exist by default upon creation of CreateDDLMySQL object.", testSQLString);
     }
 
     @Test
-    public void testDatabaseCreation(){
-
-    }
-
-    @Test
-    public void testDatabaseUsed(){
-
+    public void testDefaultDatabaseNameCreationandUse(){
+        assertEquals("Database Name recorded does not match expected default of \"MySQLDB\".","MySQLDB", testObj.getDatabaseName());
+        assertTrue("SQL string should include statement to create database. \"CREATE DATABASE MySQLDB;\"",testSQLString.contains("CREATE DATABASE MySQLDB;"));
+        assertTrue("SQL string should include statement to use database. \"USE MySQLDB;\"",testSQLString.contains("USE MySQLDB;"));
     }
 
     @Test
     public void testTableCreated(){
-
+        
     }
 
     @Test
