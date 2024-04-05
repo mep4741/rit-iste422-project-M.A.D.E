e318f53a3eb5e6b2352e6b7055cf8e4289388192
Declan Naughton <djn5293@rit.edu>
2024-04-05 16:48:11 -0400

    Coding from yesterday.

--- src/test/java/CreateDDLMySQLTest.java
+++ src/test/java/CreateDDLMySQLTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 import java.lang.reflect.Array;
+import java.util.StringTokenizer;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,25 +10,36 @@ public class CreateDDLMySQLTest {
     private CreateDDLMySQL testObj;
     private String testSQLString;
 
-
     //These tests assume that EdgeField and EdgeTable all tested correctly.
     @Before
     public void setUp() throws Exception{
         EdgeTable testTableA = new EdgeTable("1|Table A");
         EdgeTable tesTableB = new EdgeTable("2|Table B");
-        EdgeTable[] tables = new EdgeTable[]{
-            new EdgeTable("1|Table a|Field alpha|Field A"),
-            new EdgeTable("2|Table b|Field bravo|Field B")
-        };
-        EdgeField[] fields = new EdgeField[]{
+        testTableA.addNativeField(1);
+        testTableA.addNativeField(3);
+        testTableA.addNativeField(5);
+        testTableA.makeArrays();
+        testTableA.setRelatedField(0, 2);
+
+        tesTableB.addNativeField(2);
+        tesTableB.addNativeField(4);
+        tesTableB.addNativeField(6);
+        tesTableB.makeArrays();
+        tesTableB.setRelatedField(0, 1);
+        tesTableB.addRelatedTable(1);
+
+        testObj = new CreateDDLMySQL(new EdgeTable[]{testTableA,tesTableB}, 
+        new EdgeField[]{
             new EdgeField("1|Field alpha"),
             new EdgeField("2|Field bravo"),
             new EdgeField("3|Field A"),
-            new EdgeField("4|Field B")
-        };
-        testObj = new CreateDDLMySQL(tables, fields);
+            new EdgeField("4|Field B"),
+            new EdgeField("5|Field あ"),
+            new EdgeField("6|Field beta")
+        });
         testObj.createDDL();//Create String
         testSQLString = testObj.getSQLString();
+
     }//setUp
 
     @Test
@@ -36,7 +48,7 @@ public class CreateDDLMySQLTest {
     }
 
     @Test
-    public void testDefaultDatabaseNameCreationandUse(){
+    public void testDatabaseNameCreationandUse(){
         assertEquals("Database Name recorded does not match expected default of \"MySQLDB\".","MySQLDB", testObj.getDatabaseName());
         assertTrue("SQL string should include statement to create database. \"CREATE DATABASE MySQLDB;\"",testSQLString.contains("CREATE DATABASE MySQLDB;"));
         assertTrue("SQL string should include statement to use database. \"USE MySQLDB;\"",testSQLString.contains("USE MySQLDB;"));
@@ -44,7 +56,8 @@ public class CreateDDLMySQLTest {
 
     @Test
     public void testTableCreated(){
-        
+        boolean forcePrint = false;
+        assertTrue(testSQLString, forcePrint);
     }
 
     @Test
